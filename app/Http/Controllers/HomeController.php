35bfee63ef57687b48b479ed801ddfb1d7687b99
ravisoam <ravinsoam@gmail.com>
2020-12-20 17:01:21 +0530

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $test = 'Hello, This is for test';
+        
+        
+        $user = User::all();
+       
+        return view('home', compact('test', 'user'));
+    }
+}
